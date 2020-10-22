9842bb7795704aa0eb9b0d4813dcd9490fed082b
mgrouch <mgrouch@users.github.com>
2020-10-22 15:25:48 -0400

    debian

---
+++ .circleci/build-debian.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+#
+# Build for Debian in a docker container
+#
+
+# bailout on errors and echo commands.
+set -xe
+
+DOCKER_SOCK="unix:///var/run/docker.sock"
+
+echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" | sudo tee /etc/default/docker > /dev/null
+sudo service docker restart
+sleep 5;
+
+if [ "$EMU" = "on" ]; then
+  if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
+      docker run --rm --privileged multiarch/qemu-user-static:register --reset
+  else
+      docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  fi
+fi
+
+WORK_DIR=$(pwd):/ci-source
+
+docker run --privileged -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
+
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dh-exec meson cmake \
+ at-spi2-core \
+ dh-sequence-gir \
+ fonts-cantarell \
+ gnome-pkg-tools \
+ gobject-introspection \
+ gtk-doc-tools \
+ libcolord-dev \
+ libcups2-dev \
+ libgdk-pixbuf2.0-dev \
+ libgirepository1.0-dev \
+ libjson-glib-dev \
+ librest-dev \
+ libxkbfile-dev \
+ sassc \
+ xvfb \
+ libcairo2-doc \
+ xsltproc
+
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+    "cd ci-source; dpkg-buildpackage -b -uc -us; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
+
+find dist -name \*.deb
+
+echo "Stopping"
+docker ps -a
+docker stop $DOCKER_CONTAINER_ID
+docker rm -v $DOCKER_CONTAINER_ID
