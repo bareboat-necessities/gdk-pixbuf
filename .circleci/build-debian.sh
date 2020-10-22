bc8407d52cc5174486a16509b3b09cd87fac8f92
mgrouch <mgrouch@users.github.com>
2020-10-22 17:30:14 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -42,6 +42,7 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dh-exec mes
  libxkbfile-dev \
  sassc \
  xvfb \
+ gtk-doc-tools \
  libglib2.0-doc \
  libcairo2-doc \
  xsltproc
