3375beb68f399d4e58c227ea740970be2111ae42
mgrouch <mgrushinskiy@gmail.com>
2020-10-23 21:04:50 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -26,8 +26,8 @@ WORK_DIR=$(pwd):/ci-source
 docker run --privileged -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
-docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
-docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec meson cmake man \
+docker exec -ti $DOCKER_CONTAINER_ID apt-get update
+docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec meson cmake man \
     at-spi2-core                      \
     dh-sequence-gir                   \
     fonts-cantarell                   \
@@ -75,9 +75,10 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install build-essen
     libxcomposite1                    \
     libpango1.0-doc
 
-#docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get upgrade
+docker exec -ti $DOCKER_CONTAINER_ID apt-get upgrade
+docker exec -ti $DOCKER_CONTAINER_ID ldconfig
 
-docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     "cd ci-source; dpkg-buildpackage -b -uc -us; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
 
 find dist -name \*.deb
