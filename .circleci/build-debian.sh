67f087d8b66dd6fed2caabceb556ec86a750ccea
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 11:05:24 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -75,7 +75,7 @@ docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec 
     libxcomposite1                    \
     libpango1.0-doc
 
-docker exec -ti $DOCKER_CONTAINER_ID apt-get upgrade
+docker exec -ti $DOCKER_CONTAINER_ID apt-get -y upgrade
 docker exec -ti $DOCKER_CONTAINER_ID ldconfig
 
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
