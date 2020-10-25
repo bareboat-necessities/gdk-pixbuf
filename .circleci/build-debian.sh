fe0a01a9cbbddcd0bb9f829182503f1ea0c82c36
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 14:02:02 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -81,8 +81,7 @@ docker exec -ti $DOCKER_CONTAINER_ID apt-get -y upgrade
 docker exec -ti $DOCKER_CONTAINER_ID ldconfig
 
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; " \
-    "mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
+    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist "
 
 pwd
 ls
