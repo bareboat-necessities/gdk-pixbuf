e6d1aa167a664b22c76d74d7e0ed9c2cc36beca2
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 19:25:22 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -17,18 +17,18 @@ if [ "$EMU" = "on" ]; then
   if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
       docker run --rm --privileged multiarch/qemu-user-static:register --reset
   else
-      docker run --rm --privileged multiarch/qemu-user-static --reset --security-opt="seccomp=unconfined" --credential yes --persistent yes
+      docker run --rm --privileged multiarch/qemu-user-static --reset --security-opt="seccomp=unconfined" --cap-add=ALL --credential yes --persistent yes
   fi
 fi
 
 WORK_DIR=$(pwd):/ci-source
 
-docker run --privileged --security-opt="seccomp=unconfined" -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+docker run --privileged --security-opt="seccomp=unconfined" --cap-add=ALL -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
-docker exec -ti $DOCKER_CONTAINER_ID apt-get update
-docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot
-docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec meson cmake man-db \
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec meson cmake man-db \
     at-spi2-core                      \
     dh-sequence-gir                   \
     fonts-cantarell                   \
@@ -76,10 +76,10 @@ docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install build-essential dh-exec 
     libxcomposite1                    \
     libpango1.0-doc
 
-#docker exec -ti $DOCKER_CONTAINER_ID apt-get -y upgrade
-docker exec -ti $DOCKER_CONTAINER_ID ldconfig
+#docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y upgrade
+docker exec --privileged -ti $DOCKER_CONTAINER_ID ldconfig
 
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist "
 
 find dist -name \*.\*$EXT
