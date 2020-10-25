9d99b1754e7a4787f455444d7fc31aa16db9de1f
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 16:07:25 -0400

    deb

--- .circleci/publish-cloudsmith.sh
+++ .circleci/publish-cloudsmith.sh
@@ -6,4 +6,8 @@ DISTRO=$3
 
 for pkg_file in dist/*.$EXT; do
   cloudsmith push deb $REPO/$DISTRO $pkg_file
+  RESULT=$?
+  if [[ $pkg_file == "*all.deb" && $RESULT -ne 0 ]] ; then
+     echo " skipping already deployed $pkg_file"
+  fi
 done
