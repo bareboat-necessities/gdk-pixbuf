b7d49d4f91f2248b2b2996387f364d251c923979
mgrouch <mgrushinskiy@gmail.com>
2020-10-26 10:27:22 -0400

    deb

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -17,7 +17,7 @@ if [ "$EMU" = "on" ]; then
   if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
       docker run --rm --privileged multiarch/qemu-user-static:register --reset
   else
-      docker run --rm --privileged multiarch/qemu-user-static --reset --security-opt="seccomp=unconfined" --cap-add=ALL --credential yes --persistent yes
+      docker run --rm --privileged --security-opt="seccomp=unconfined" --cap-add=ALL multiarch/qemu-user-static --reset --credential yes --persistent yes
   fi
 fi
 
